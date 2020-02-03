fe1d3027dd402a8590c1f693602c09ee3a9ac201
bratay <violinbt2@yahoo.com>
2020-02-03 14:42:21 -0600

    Load needed models

--- CompareMLModels.py
+++ CompareMLModels.py
@@ -1,7 +1,5 @@
 # Load libraries
 from pandas import read_csv
-from pandas.plotting import scatter_matrix
-from matplotlib import pyplot
 from sklearn.model_selection import train_test_split
 from sklearn.model_selection import cross_val_score
 from sklearn.model_selection import StratifiedKFold
@@ -9,8 +7,40 @@ from sklearn.metrics import classification_report
 from sklearn.metrics import confusion_matrix
 from sklearn.metrics import accuracy_score
 from sklearn.linear_model import LogisticRegression
-from sklearn.tree import DecisionTreeClassifier
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 from sklearn.naive_bayes import GaussianNB
-from sklearn.svm import SVC
+
+
+# Load dataset
+url = "https://raw.githubusercontent.com/jbrownlee/Datasets/master/iris.csv"
+names = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'class']
+dataset = read_csv(url, names=names)
+
+array = dataset.values
+X = [:,0:4]
+y = [:,4]
+X_train, X_validation, Y_train, Y_validation = train_test_split(X, y, test_size=0.50, random_state=1)
+
+models = []
+# Linear regression (LinearRegression)
+models.append(('LinearRegression', LogisticRegression(solver='liblinear', multi_class='ovr')))
+# Polynomial of degree 2 regression (LinearRegression)
+models.append(('LinearRegression degree 2', LogisticRegression(solver='liblinear', multi_class='ovr')))
+# Polynomial of degree 3 regression (LinearRegression)
+models.append(('LinearRegression degree 3', LogisticRegression(solver='liblinear', multi_class='ovr')))
+# kNN (KNeighborsClassifier)
+models.append(('KNeighborsClassifier', KNeighborsClassifier()))
+# LDA (LinearDiscriminantAnalysis)
+models.append(('LinearDiscriminantAnalysis', LinearDiscriminantAnalysis()))
+#Naïve Baysian (NBClassifier)
+models.append(('NBClassifier', GaussianNB()))# Same as NBClassifier?
+
+results = []
+names = []
+for model in models:
+    #train, test, and get result
+    
+    
+    
+#print
