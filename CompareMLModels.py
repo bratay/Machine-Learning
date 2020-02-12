34c67899bc0fafd026426e480f6af93218ec0a44
bratay <violinbt2@yahoo.com>
2020-02-11 22:55:47 -0600

    Concatenated both predictions for better results

--- CompareMLModels.py
+++ CompareMLModels.py
@@ -11,22 +11,30 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 from sklearn.naive_bayes import GaussianNB
 
+import numpy as np
 
 def printResults(modelData, modelName):
     print("#######################################")
     print("%s" % modelName)
     print("#######################################")
+    
     # Make prediction on validation dataset
-    modelData.fit(X_train, Y_train)
-    prediction = modelData.predict(X_validation)
+    modelData.fit(x_one, y_one)
+    prediction1 = modelData.predict(x_two)
+
+    modelData.fit(x_two, y_two)
+    prediction2 = modelData.predict(x_one)
+
+    #combined both predictions
+    prediction = np.concatenate((prediction1, prediction2))
+    val = np.concatenate((y_two, y_one))
 
     # Evaluate prediction
     print("Accuracy metric")
-    print(accuracy_score(Y_validation, prediction))
+    print(accuracy_score(val, prediction))
     print("\nConfusion matrix")
-    print(confusion_matrix(Y_validation, prediction))
+    print(confusion_matrix(val, prediction))
     print("\n")
-
 
 # Load dataset
 url = "https://raw.githubusercontent.com/jbrownlee/Datasets/master/iris.csv"
@@ -34,10 +42,10 @@ names = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'class']
 dataset = read_csv(url, names=names)
 
 array = dataset.values
-X = array[:, 0:4]
+x = array[:, 0:4]
 y = array[:, 4]
-X_train, X_validation, Y_train, Y_validation = train_test_split(
-    X, y, test_size=0.50, random_state=1)
+x_one, x_two, y_one, y_two = train_test_split(
+    x, y, test_size=0.50, random_state=1)
 
 models = []
 # Linear regression (LinearRegression)
@@ -56,14 +64,14 @@ models.append(('LinearDiscriminantAnalysis', LinearDiscriminantAnalysis()))
 # Naive Baysian (NBClassifier)
 models.append(('NBClassifier', GaussianNB()))  # Same as NBClassifier?
 
-results = []
 names = []
-#Train make prediction and print result
+# Train make prediction and print result
 for name, model in models:
     twoFold = StratifiedKFold(n_splits=2, random_state=1, shuffle=True)
+
     cv_results = cross_val_score(
-        model, X_train, Y_train, cv=twoFold, scoring='accuracy')
-    results.append(cv_results)
+        model, x_one, y_one, cv=twoFold, scoring='accuracy')
+
     names.append(name)
 
     printResults(model, name)
