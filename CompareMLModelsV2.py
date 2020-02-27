ed7593cc71f1f4aa1e13053a095bc0705f5c0b73
bratay <violinbt2@yahoo.com>
2020-02-27 12:42:26 -0600

    Refractor Polynomial Regressions

--- CompareMLModelsV2.py
+++ CompareMLModelsV2.py
@@ -21,6 +21,7 @@ from sklearn.preprocessing import PolynomialFeatures
 from sklearn import svm
 from sklearn.neural_network import MLPClassifier
 
+
 def printLinearResults(matrix, acc, modelName):
     print("#######################################")
     print("%s" % modelName)
@@ -117,10 +118,36 @@ model.fit(X_train2, y_train2)
 pred_fold2 = model.predict(X_validation2)
 pred_fold2 = pred_fold2.round()
 tmpAccuracy = accuracy_score(final, np.concatenate([pred_fold1, pred_fold2]))
-matrix = confusion_matrix(np.concatenate([y_validation1, y_validation2]), np.concatenate([pred_fold1, pred_fold2]))
+matrix = confusion_matrix(np.concatenate(
+    [y_validation1, y_validation2]), np.concatenate([pred_fold1, pred_fold2]))
 
-printLinearResults( matrix , str(round(tmpAccuracy, 3)) , "Linear Regression: ")
+printLinearResults(matrix, str(round(tmpAccuracy, 3)), "Linear Regression: ")
 
+array = dataset.values
+X = array[:, 0:4]
+y = array[:, 4]
+
+X_trainFold1, X_validationFold1, y_trainFold1, y_validationFold1 = train_test_split(
+    X, y, test_size=0.50, random_state=1)
+X_trainFold2 = X_validationFold1
+X_validationFold2 = X_trainFold1
+y_trainFold2 = y_validationFold1
+y_validationFold2 = y_trainFold1
+
+finalValidation = np.concatenate((y_validationFold1, y_validationFold2))
+
+array2 = preprocessing.LabelEncoder()
+array2.fit(y)
+array2.transform(y)
+
+X_train1, X_validation1, y_train1, y_validation1 = train_test_split(
+    X, array2.transform(y), test_size=0.50, random_state=1)
+X_train2 = X_validation1
+X_validation2 = X_train1
+y_train2 = y_validation1
+y_validation2 = y_train1
+
+final = np.concatenate((y_validation1, y_validation2))
 
 # 2 Degree Polynomial
 model = linear_model.LinearRegression()
@@ -128,22 +155,29 @@ deg2 = PolynomialFeatures(degree=2)
 deg2_train = deg2.fit_transform(X_train2)
 deg2_valid = deg2.fit_transform(X_validation2)
 model.fit(deg2_train, y_train2)
-foldOne = model.predict(deg2_valid)
-foldOne = foldOne.round()
-foldOne = np.where(foldOne >= 3.0, 2.0, foldOne)
-foldOne = np.where(foldOne <= -1.0, 0.0, foldOne)
+preds_fold1 = model.predict(deg2_valid)
+preds_fold1 = preds_fold1.round()
+preds_fold1 = np.where(preds_fold1 >= 3.0, 2.0, preds_fold1)
+preds_fold1 = np.where(preds_fold1 <= -1.0, 0.0, preds_fold1)
 model.fit(deg2_valid, y_validation2)
-foldTwo = model.predict(deg2_train)
-foldTwo = foldTwo.round()
-foldTwo = np.where(foldTwo >= 3.0, 2.0, foldTwo)
-foldTwo = np.where(foldTwo <= -1.0, 0.0, foldTwo)
+preds_fold2 = model.predict(deg2_train)
+preds_fold2 = preds_fold2.round()
+preds_fold2 = np.where(preds_fold2 >= 3.0, 2.0, preds_fold2)
+preds_fold2 = np.where(preds_fold2 <= -1.0, 0.0, preds_fold2)
 tmpAccuracy = accuracy_score(
-    final, np.concatenate([foldOne, foldTwo]))
-matrix = confusion_matrix(np.concatenate(
-    [y_validation2, y_train2]), np.concatenate([foldOne, foldTwo]))
+    final, np.concatenate([pred_fold1, pred_fold2]))
+tmpAccuracy = round(tmpAccuracy, 3)
 
-printLinearResults( matrix , str(round(tmpAccuracy, 3)) , "2 Degree Polynomial Regression")
+print("#######################################")
+print("2 Degree Polynomial Regression")
+print("#######################################")
 
+# Evaluate prediction
+print("Accuracy metric: " + str(tmpAccuracy))
+print("\nConfusion matrix")
+print(confusion_matrix(np.concatenate(
+    [y_validation2, y_train2]), np.concatenate([preds_fold1, preds_fold2])))
+print("\n")
 
 # 3 Degree Polynomial
 model = linear_model.LinearRegression()
@@ -151,18 +185,26 @@ deg3 = PolynomialFeatures(degree=3)
 deg3_train = deg3.fit_transform(X_train2)
 deg3_valid = deg3.fit_transform(X_validation2)
 model.fit(deg3_train, y_train2)
-foldOne = model.predict(deg3_valid)
-foldOne = foldOne.round()
-foldOne = np.where(foldOne >= 3.0, 2.0, foldOne)
-foldOne = np.where(foldOne <= -1.0, 0.0, foldOne)
+preds_fold1 = model.predict(deg3_valid)
+preds_fold1 = preds_fold1.round()
+preds_fold1 = np.where(preds_fold1 >= 3.0, 2.0, preds_fold1)
+preds_fold1 = np.where(preds_fold1 <= -1.0, 0.0, preds_fold1)
 model.fit(deg3_valid, y_validation2)
-foldTwo = model.predict(deg3_train)
-foldTwo = foldTwo.round()
-foldTwo = np.where(foldTwo >= 3.0, 2.0, foldTwo)
-foldTwo = np.where(foldTwo <= -1.0, 0.0, foldTwo)
+preds_fold2 = model.predict(deg3_train)
+preds_fold2 = preds_fold2.round()
+preds_fold2 = np.where(preds_fold2 >= 3.0, 2.0, preds_fold2)
+preds_fold2 = np.where(preds_fold2 <= -1.0, 0.0, preds_fold2)
 tmpAccuracy = accuracy_score(
-    final, np.concatenate([foldOne, foldTwo]))
-matrix = confusion_matrix(np.concatenate(
-    [y_validation2, y_train2]), np.concatenate([foldOne, foldTwo]))
+    final, np.concatenate([preds_fold1, preds_fold2]))
+tmpAccuracy = round(tmpAccuracy, 3)
 
-printLinearResults( matrix , str(round(tmpAccuracy, 3)) , "3 Degree Polynomial Regression")
+print("#######################################")
+print("3 Degree Polynomial Regression")
+print("#######################################")
+
+# Evaluate prediction
+print("Accuracy Score: " + str(tmpAccuracy))
+print('Confusion Matrix:')
+print(confusion_matrix(np.concatenate(
+    [y_validation2, y_train2]), np.concatenate([preds_fold1, preds_fold2])))
+print("\n")
