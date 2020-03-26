98277a0378f9d43f11a3d6fe391f12f548227a50
bratay <violinbt2@yahoo.com>
2020-03-26 17:26:25 -0500

    New model trained

--- DecisionTreeFeatureSelection.py
+++ DecisionTreeFeatureSelection.py
@@ -31,9 +31,9 @@ X_train, X_validation, Y_train, Y_validation = train_test_split(
     X, y, test_size=0.20, random_state=1)
 
 # training model
-# 'Gaussian Model', GaussianNB()
 twoFold = StratifiedKFold(n_splits=2, random_state=1, shuffle=True)
-result = cross_val_score( DecisionTreeClassifier(), X_train, Y_train, cv = twoFold, scoring='accuracy')
+result = cross_val_score(DecisionTreeClassifier(
+), X_train, Y_train, cv=twoFold, scoring='accuracy')
 
 print('%s: %f (%f)' % ('Decision Tree', result.mean(), result.std()))
 
@@ -48,24 +48,47 @@ print(confusion_matrix(Y_validation, predictions))
 print(classification_report(Y_validation, predictions))
 
 #############################################
-##Part 2
+# Part 2
 #############################################
+print("\n\n//////////////////  PART 2 ///////////////\n\n")
 
 # define a matrix
 A = X
-
 # calculate the mean of each column
 M = mean(A.T, axis=1)
-
 # center columns by subtracting column means
 C = A - M
-
 # calculate covariance matrix of centered matrix
 V = cov(C.T.astype(float))
-
 # eigendecomposition of covariance matrix
 values, vectors = eig(V)
 print("/////////////" + " eigen values " + "/////////////")
 print(vectors)
-print("/////////////" + " eigen vectors " + "/////////////")
+print("\n/////////////" + " eigen vectors " + "/////////////")
 print(values)
+
+print('\nPoV = ' + str(values[0] / (values[0] + values[1] + values[2])) + "\n")
+
+# Split-out validation dataset
+array = dataset.values
+X = array[:, 0:3]
+y = array[:, 4]
+X_train, X_validation, Y_train, Y_validation = train_test_split(
+    X, y, test_size=0.20, random_state=1)
+
+# training model
+twoFold = StratifiedKFold(n_splits=2, random_state=1, shuffle=True)
+result = cross_val_score(DecisionTreeClassifier(
+), X_train, Y_train, cv=twoFold, scoring='accuracy')
+
+print('%s: %f (%f)' % ('Decision Tree', result.mean(), result.std()))
+
+# Make predictions on validation dataset
+model = DecisionTreeClassifier()
+model.fit(X_train, Y_train)
+predictions = model.predict(X_validation)
+
+# Evaluate predictions
+print(accuracy_score(Y_validation, predictions))
+print(confusion_matrix(Y_validation, predictions))
+print(classification_report(Y_validation, predictions))
