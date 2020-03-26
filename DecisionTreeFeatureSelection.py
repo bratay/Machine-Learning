dc3e20eb423c7e9a0b641069cf8b95c04b73cccb
bratay <violinbt2@yahoo.com>
2020-03-26 16:29:40 -0500

    Principle Component Analysis example

--- DecisionTreeFeatureSelection.py
+++ DecisionTreeFeatureSelection.py
@@ -11,6 +11,12 @@ from sklearn.metrics import accuracy_score
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 
+import numpy as np
+from numpy import array
+from numpy import mean
+from numpy import cov
+from numpy.linalg import eig
+
 
 # Load dataset
 url = "https://raw.githubusercontent.com/jbrownlee/Datasets/master/iris.csv"
@@ -40,3 +46,27 @@ predictions = model.predict(X_validation)
 print(accuracy_score(Y_validation, predictions))
 print(confusion_matrix(Y_validation, predictions))
 print(classification_report(Y_validation, predictions))
+
+#############################################
+##Part 2
+#############################################
+
+# define a matrix
+A = np.array([[1, 2], [3, 4], [5, 6]], np.int32)
+print(A)
+# calculate the mean of each column
+M = mean(A.T, axis=1)
+print(M)
+# center columns by subtracting column means
+C = A - M
+print(C)
+# calculate covariance matrix of centered matrix
+V = cov(C.T)
+print(V)
+# eigendecomposition of covariance matrix
+values, vectors = eig(V)
+print(vectors)
+print(values)
+# project data
+P = vectors.T.dot(C.T)
+print(P.T)
