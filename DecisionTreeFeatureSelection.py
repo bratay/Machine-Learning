93fdb7d7e8f5fc6f443b78cacf6764756a0413c0
bratay <violinbt2@yahoo.com>
2020-03-25 22:47:29 -0500

    Decision Tree base case

--- DecisionTreeFeatureSelection.py
+++ DecisionTreeFeatureSelection.py
@@ -10,3 +10,33 @@ from sklearn.metrics import confusion_matrix
 from sklearn.metrics import accuracy_score
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+
+
+# Load dataset
+url = "https://raw.githubusercontent.com/jbrownlee/Datasets/master/iris.csv"
+names = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'class']
+dataset = read_csv(url, names=names)
+
+# Split-out validation dataset
+array = dataset.values
+X = array[:, 0:4]
+y = array[:, 4]
+X_train, X_validation, Y_train, Y_validation = train_test_split(
+    X, y, test_size=0.20, random_state=1)
+
+# training model
+# 'Gaussian Model', GaussianNB()
+twoFold = StratifiedKFold(n_splits=2, random_state=1, shuffle=True)
+result = cross_val_score( DecisionTreeClassifier(), X_train, Y_train, cv = twoFold, scoring='accuracy')
+
+print('%s: %f (%f)' % ('Decision Tree', result.mean(), result.std()))
+
+# Make predictions on validation dataset
+model = DecisionTreeClassifier()
+model.fit(X_train, Y_train)
+predictions = model.predict(X_validation)
+
+# Evaluate predictions
+print(accuracy_score(Y_validation, predictions))
+print(confusion_matrix(Y_validation, predictions))
+print(classification_report(Y_validation, predictions))
