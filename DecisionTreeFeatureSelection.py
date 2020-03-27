48620f26a62e5b8186fe1e889f74f999b4b6c830
bratay <violinbt2@yahoo.com>
2020-03-26 22:51:34 -0500

    Two fold cross-validation refactor

--- DecisionTreeFeatureSelection.py
+++ DecisionTreeFeatureSelection.py
@@ -17,46 +17,54 @@ from numpy import mean
 from numpy import cov
 from numpy.linalg import eig
 
-
 # Load dataset
 url = "https://raw.githubusercontent.com/jbrownlee/Datasets/master/iris.csv"
 names = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'class']
 dataset = read_csv(url, names=names)
 
-# Split-out validation dataset
+#############################################
+# Part 1
+#############################################
+print("\n\n////////////////// PART 1 //////////////////\n\n")
+
+
+def printResults(modelData):
+    # Make prediction on validation dataset
+    modelData.fit(x_one, y_one)
+    prediction1 = modelData.predict(x_two)
+
+    modelData.fit(x_two, y_two)
+    prediction2 = modelData.predict(x_one)
+
+    # combined both predictions
+    prediction = np.concatenate((prediction1, prediction2))
+    val = np.concatenate((y_two, y_one))
+    # prediction = tempPrediction
+
+    # Evaluate prediction
+    print("Accuracy metric")
+    print(accuracy_score(val, prediction))
+    print("\nConfusion matrix")
+    print(confusion_matrix(val, prediction))
+
 array = dataset.values
-X = array[:, 0:4]
+x = array[:, 0:4]
 y = array[:, 4]
-X_train, X_validation, Y_train, Y_validation = train_test_split(
-    X, y, test_size=0.20, random_state=1)
+x_one, x_two, y_one, y_two = train_test_split(
+    x, y, test_size=0.50, random_state=1)
 
-# training model
 twoFold = StratifiedKFold(n_splits=2, random_state=1, shuffle=True)
-result = cross_val_score(DecisionTreeClassifier(
-), X_train, Y_train, cv=twoFold, scoring='accuracy')
-
-print('%s: %f (%f)' % ('Decision Tree', result.mean(), result.std()))
-
-# Make predictions on validation dataset
-model = DecisionTreeClassifier()
-model.fit(X_train, Y_train)
-predictions = model.predict(X_validation)
-
-# Evaluate predictions
-print(accuracy_score(Y_validation, predictions))
-print(confusion_matrix(Y_validation, predictions))
-print(classification_report(Y_validation, predictions))
+printResults(DecisionTreeClassifier())
 
 #############################################
-# Part 2
+# Part 2 PCA
 #############################################
-print("\n\n//////////////////  PART 2 ///////////////\n\n")
+print("\n\n//////////////////  PART 2 //////////////////\n\n")
 
 # define a matrix
-A = X
-# calculate the mean of each column
+A = x
+# calculate the mean of each column and center columns
 M = mean(A.T, axis=1)
-# center columns by subtracting column means
 C = A - M
 # calculate covariance matrix of centered matrix
 V = cov(C.T.astype(float))
@@ -69,26 +77,62 @@ print(values)
 
 print('\nPoV = ' + str(values[0] / (values[0] + values[1] + values[2])) + "\n")
 
+# For part 3 and 4
+P = vectors.T.dot(C.T)
+newFeatures = P.T
+
+# used in part 4
+transformedFeatures = vectors.T.dot(C.T)
+
 # Split-out validation dataset
 array = dataset.values
-X = array[:, 0:3]
+x = array[:, 0:3]
 y = array[:, 4]
-X_train, X_validation, Y_train, Y_validation = train_test_split(
-    X, y, test_size=0.20, random_state=1)
+x_one, x_two, y_one, y_two = train_test_split(
+    x, y, test_size=0.50, random_state=1)
 
-# training model
 twoFold = StratifiedKFold(n_splits=2, random_state=1, shuffle=True)
-result = cross_val_score(DecisionTreeClassifier(
-), X_train, Y_train, cv=twoFold, scoring='accuracy')
+printResults(DecisionTreeClassifier())
 
-print('%s: %f (%f)' % ('Decision Tree', result.mean(), result.std()))
+#############################################
+# Part 3 simulated annealing
+#############################################
+print("\n\n////////////////// PART 3 //////////////////\n\n")
 
-# Make predictions on validation dataset
-model = DecisionTreeClassifier()
-model.fit(X_train, Y_train)
-predictions = model.predict(X_validation)
+array = dataset.values
+x = array[:, 0:4]
+y = array[:, 4]
 
-# Evaluate predictions
-print(accuracy_score(Y_validation, predictions))
-print(confusion_matrix(Y_validation, predictions))
-print(classification_report(Y_validation, predictions))
+allData8F = np.concatenate((newFeatures, x), axis=1)
+# print(allData8F)
+
+X = allData8F[:, 0:8]
+print(X)
+
+curPerformance = 0
+prePreformance = 0
+for x in range(0, 100):
+    # fit model
+    X_train, X_validation, Y_train, Y_validation = train_test_split(
+        X, y, test_size=0.50, random_state=1)
+    curPerformance = 1  # estimate performance
+    if(curPerformance > prePreformance):
+        # print("ACCEPT")
+        prePreformance = curPerformance
+    else:
+        acceptProbability = 0
+        ranVar = 0
+
+        if(ranVar > acceptProbability):
+            # print("REJECT")
+            curPerformance = 0
+        else:
+            # print("ACCEPT")
+            prePreformance = curPerformance
+
+
+#############################################
+# Part 4 simulated annealing
+#############################################
+print("\n\n////////////////// PART 4 //////////////////\n\n")
+
