c98cf6c741501bae883560f92a356a27622a3d13
bratay <violinbt2@yahoo.com>
2020-04-09 22:09:19 -0500

    Oversampling import

--- imblancedDataSet/imbalancedDataSet.py
+++ imblancedDataSet/imbalancedDataSet.py
@@ -1,4 +1,6 @@
 # Load libraries
+from collections import Counter
+from sklearn.datasets import make_classification
 from pandas import read_csv
 from pandas.plotting import scatter_matrix
 from matplotlib import pyplot
@@ -17,6 +19,12 @@ from numpy import array
 from numpy import mean
 from numpy import cov
 from numpy.linalg import eig
+from sklearn.datasets import make_classification
+
+# from imblearn import under_sampling, over_sampling
+# from imblearn.over_sampling import SMOTE
+# from imblearn.over_sampling import RandomOverSampler
+
 
 
 # Load dataset
@@ -78,26 +86,40 @@ print(confusion_matrix(val, prediction))
 print("Class balance accuracy")
 matrix = confusion_matrix(val, prediction)
 
-classOne = float(min(matrix[0][0] / float(matrix[0][0] + matrix[0][1] + matrix[0][2] ) , matrix[0][0] / (matrix[0][0] + matrix[1][0] + matrix[2][0] ) ))
-classTwo = float(min(matrix[1][1] / float((matrix[1][1] + matrix[1][0] + matrix[1][2] )), matrix[1][1] / float((matrix[1][0] + matrix[1][1] + matrix[1][2] ) )))
-classThree = float(min(matrix[2][2] / float((matrix[2][0] + matrix[2][1] + matrix[2][2] ) ), matrix[2][2] / float((matrix[0][2] + matrix[1][2] + matrix[2][2] ) )))
+classOne = float(min(matrix[0][0] / float(matrix[0][0] + matrix[0][1] +
+                                          matrix[0][2]), matrix[0][0] / (matrix[0][0] + matrix[1][0] + matrix[2][0])))
+classTwo = float(min(matrix[1][1] / float((matrix[1][1] + matrix[1][0] + matrix[1][2])),
+                     matrix[1][1] / float((matrix[1][0] + matrix[1][1] + matrix[1][2]))))
+classThree = float(min(matrix[2][2] / float((matrix[2][0] + matrix[2][1] + matrix[2][2])),
+                       matrix[2][2] / float((matrix[0][2] + matrix[1][2] + matrix[2][2]))))
 
-print( (classOne + classTwo + classThree ) / 3 )
+print((classOne + classTwo + classThree) / 3)
 
 print("Balance accuracy")
-# print( np.sum( matrix, axis = 0)[2])
-# print(float( np.sum( matrix, axis = 0)[2] + np.sum( matrix, axis = 0)[1] - matrix[0][1] - matrix[0][2] ) )
-classOne = float(min( float( np.sum( matrix, axis = 0)[2] + np.sum( matrix, axis = 0)[1] - matrix[0][1] - matrix[0][2] ) /  (float( np.sum( matrix, axis = 0)[2] + np.sum( matrix, axis = 0)[1] ) ) , matrix[0][0] / (matrix[0][0] + matrix[1][0] + matrix[2][0] ) ))
-classTwo = float(min( float( np.sum( matrix, axis = 0)[2] + np.sum( matrix, axis = 0)[0] - matrix[1][0] - matrix[1][2] ) /  (float( np.sum( matrix, axis = 0)[2] + np.sum( matrix, axis = 0)[0] ) )  , matrix[1][1] / float((matrix[1][0] + matrix[1][1] + matrix[1][2] ) )))
-classThree = float(min(float( np.sum( matrix, axis = 0)[0] + np.sum( matrix, axis = 0)[1] - matrix[2][1] - matrix[2][0] ) /  (float( np.sum( matrix, axis = 0)[0] + np.sum( matrix, axis = 0)[1] ) ) , matrix[2][2] / float((matrix[0][2] + matrix[1][2] + matrix[2][2] ) )))
+classOne = float(min(float(np.sum(matrix, axis=0)[2] + np.sum(matrix, axis=0)[1] - matrix[0][1] - matrix[0][2]) / (float(
+    np.sum(matrix, axis=0)[2] + np.sum(matrix, axis=0)[1])), matrix[0][0] / (matrix[0][0] + matrix[1][0] + matrix[2][0])))
+classTwo = float(min(float(np.sum(matrix, axis=0)[2] + np.sum(matrix, axis=0)[0] - matrix[1][0] - matrix[1][2]) / (float(
+    np.sum(matrix, axis=0)[2] + np.sum(matrix, axis=0)[0])), matrix[1][1] / float((matrix[1][0] + matrix[1][1] + matrix[1][2]))))
+classThree = float(min(float(np.sum(matrix, axis=0)[0] + np.sum(matrix, axis=0)[1] - matrix[2][1] - matrix[2][0]) / (float(
+    np.sum(matrix, axis=0)[0] + np.sum(matrix, axis=0)[1])), matrix[2][2] / float((matrix[0][2] + matrix[1][2] + matrix[2][2]))))
 
-print( (classOne + classTwo + classThree ) / 3 )
+print((classOne + classTwo + classThree) / 3)
 
 
 #############################################
-# Part 2 PCA
+# Part 2 Oversampling
 #############################################
 print("\n\n//////////////////  PART 2 //////////////////\n\n")
+
+X, y = make_classification(n_samples=5000, n_features=2, n_informative=2,
+                           n_redundant=0, n_repeated=0, n_classes=3,
+                           n_clusters_per_class=1,
+                           weights=[0.01, 0.05, 0.94],
+                           class_sep=0.8, random_state=0)
+
+# ros = RandomOverSampler(random_state=0)
+# X_resampled, y_resampled = ros.fit_resample(X, y)
+# print(sorted(Counter(y_resampled).items()))
 
 
 #############################################
@@ -105,7 +127,5 @@ print("\n\n//////////////////  PART 2 //////////////////\n\n")
 #############################################
 print("\n\n////////////////// PART 3 //////////////////\n\n")
 
-#############################################
-# Part 4
-#############################################
-print("\n\n////////////////// PART 4 //////////////////\n\n")
+
+
