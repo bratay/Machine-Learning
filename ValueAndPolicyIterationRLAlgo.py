ec4cd6c7286e639f22921ab2054237f47807508a
bratay <violinbt2@yahoo.com>
2020-05-07 18:37:27 -0500

    Clean Code

--- ValueAndPolicyIterationRLAlgo.py
+++ ValueAndPolicyIterationRLAlgo.py
@@ -1,14 +1,9 @@
 # Load libraries
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import confusion_matrix
-from sklearn.metrics import accuracy_score
 import matplotlib.pyplot as plt
 from pandas import read_csv
 from numpy import array
-from numpy import mean
 from numpy import cov
 import numpy as np
-import csv
 
 ########################################
 # Policy Iteration RL Algorithm
@@ -30,7 +25,6 @@ while():
 
     # get q+1
     nextQ = 0
-
 
 ########################################
 # Value Iteration RL Algorithm
