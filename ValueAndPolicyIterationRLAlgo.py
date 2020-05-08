380671f2f483a8bdf0ce957daf12cc7eba4413d3
bratay <violinbt2@yahoo.com>
2020-05-07 23:15:47 -0500

    Clean Code

--- ValueAndPolicyIterationRLAlgo.py
+++ ValueAndPolicyIterationRLAlgo.py
@@ -13,97 +13,46 @@ import random
 # # Policy Iteration RL Algorithm
 # ########################################
 
-# # each state has 4 possible next states 4 directions:
-# position = [0,1]
-# grid = [[0, -1, -1, -1, -1],
-#         [-1, -1, -1, -1, -1],
-#         [-1, -1, -1, -1, -1],
-#         [-1, -1, -1, -1, -1],
-#         [-1, -1, -1, -1, 0]]
+vFunction = np.zeros((5, 5))
+deltas = {(i, j): list() for i in range(5) for j in range(5)}
+states = [[i, j] for i in range(5) for j in range(5)]
+actions = [[-1, 0], [1, 0], [0, 1], [0, -1]]
 
-# # get Vπ(s) in two parts
-# Vs = 0
 
-# # First
-# # multiplying each possible reward by its probability and adding them together
-# # Rt·p(rup|st,at) + Rt·p(rdown|st,at) + Rt·p(rright|st,at) + Rt·p(rleft|st,at)
-# # for all actions p(rt+1|st,at) = 0.25
-# # the first: -1·0.25 + -1·0.25 + -1·0.25 + -1·0.25 = -1
-# avgReward = -1
+def takeAction(state, action):
+    if list(state) == [0, 0] or list(state) == [4, 4]:
+        return 0, None
+    f = np.array(state)+np.array(action)
 
-# # Second
-# # multiplying each of the neighboring Vπ(s’) by its probability and adding them together
-# # p(st+1|st,at) = 0.25 for all actions and that γ = 1.
-# # SO: Vπ(s’up)·0.25 + Vπ(s’down)·0.25 + Vπ(s’right)·0.25+ Vπ(s’left)·0.25
-# discountedAvg = 0
+    # Out of bounds
+    if -1 in list(f) or 5 in list(f):
+        f = state
+    return -1, list(f)
 
-# Vs = avgReward + discountedAvg
 
-# # repeat 1 and 2
-# while():
-#     # multiplying each possible reward by its probability and adding them together
-#     # Rt·p(rup|st,at) + Rt·p(rdown|st,at) + Rt·p(rright|st,at) + Rt·p(rleft|st,at)
-#     avgReward = -1
+for i in range(0, 10000):
+    state = random.choice(states[1:-1])
 
-#     # multiplying each of the neighboring Vπ(s’) by its probability and adding them together
-#     # Vπ(s’up)·0.25 + Vπ(s’down)·0.25 + Vπ(s’right)·0.25+ Vπ(s’left)·0.25
-#     discountedAvg = 0
+    while True:
+        action = random.choice(actions)
+        reward, f = takeAction(state, action)
 
-#     Vs = avgReward + discountedAvg
+        if f is None:
+            break
+
+        pre = vFunction[state[0], state[1]]
+        vFunction[state[0], state[1]] += .5 * \
+            (reward + .5*vFunction[f[0], f[1]] -
+             vFunction[state[0], state[1]])
+
+        deltas[state[0], state[1]].append(
+            float(np.abs(pre-vFunction[state[0], state[1]])))
+
+        state = f
+
+print(vFunction)
+
 
 # ########################################
 # # Value Iteration RL Algorithm
 # ########################################
-
-gamma = 0.5
-alpha = 0.5
-terminationStates = [[0, 0], [4, 4]]
-actions = [[-1, 0], [1, 0], [0, 1], [0, -1]]
-numIterations = 10000
-
-
-vFunction = np.zeros((5, 5))
-deltas = {(i, j): list() for i in range(5) for j in range(5)}
-states = [[i, j] for i in range(5) for j in range(5)]
-
-
-def generateInitialState():
-    initial = random.choice(states[1:-1])
-    return initial
-
-
-def generateNextAction():
-    return random.choice(actions)
-
-
-def takeAction(state, action):
-    if list(state) in terminationStates:
-        return 0, None
-    final = np.array(state)+np.array(action)
-
-    # Out of bounds
-    if -1 in list(final) or 5 in list(final):
-        final = state
-    return -1, list(final)
-
-
-for it in range(0, numIterations):
-    state = generateInitialState()
-    while True:
-        action = generateNextAction()
-        reward, final = takeAction(state, action)
-
-        if final is None:
-            break
-
-        before = vFunction[state[0], state[1]]
-        vFunction[state[0], state[1]] += .5 * \
-            (reward + .5*vFunction[final[0], final[1]] -
-             vFunction[state[0], state[1]])
-        deltas[state[0], state[1]].append(
-            float(np.abs(before-vFunction[state[0], state[1]])))
-
-        state = final
-
-
-print(vFunction)
